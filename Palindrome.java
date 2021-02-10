2785b62a4ae274df611840f5a5210c8744563880
Malick-Issa-Sow <68846909+Malick-Issa-Sow@users.noreply.github.com>
2021-02-10 13:04:00 +0000

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+class Palindrome 
+    static boolean estPale(String mot){
+  int longueur = mot.length();booleen resultat = true;
+        i = 0;
+        while (i < longueur/ 2 && resultat){ resultat = (mot.charAt(i) == mot.charAt(longueur - 1 -i));
+            ii++;return result;}
+
+public stati String main(Strings[] args){
+ string[] affichage=null;
+   affichage[1]=" est un palindrome"
+ affichage[2]=" n'est pas un palindrome";
+   
+        for (int i=O;i <= arg.length;l++){ System.out.print((args[i]);
+ 
+ 
+            if (estPal(args[i])) { System.out.print1n(affichage[1]);
+  else{ System.out.println(affichage[2]);
+            }
+       }}
+       return "OK, je termine.";
+    }
+}
